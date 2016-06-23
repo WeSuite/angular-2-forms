5e24e5f280b1225372e405e1ab7e207846cbf5ab
Stephen Roth <stephenroth.sr@gmail.com>
2016-06-23 11:37:03 -0400

    Export validator

--- app/usernameValidators.ts
+++ app/usernameValidators.ts
@@ -1,6 +1,6 @@
 import {Control} from 'angular2/common'
 
-class UsernameValidators {
+ export class UsernameValidators {
     static cannotContainSpace(control: Control){
         if (control.value.indexOf(' ') >= 0) {
             return { cannotContainSpace: true };
